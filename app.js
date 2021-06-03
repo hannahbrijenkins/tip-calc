a47401d4f89d8bc655fb008bfdcc1dbdce65c212
HannahBri <hannahbrittany@hotmail.com>
2021-06-02 21:19:20 -0400

    converted numbers to currency using numberformat

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 // when a use inputs a dollar amount, they will select the percentage they want to tip, and when it is clicked, their total amount and tip amount will be displayed. The form can be cleared by clicking a button
 
 function getTip() {
-    // get dollar amount
-    var amountBefore = document.getElementById('billAmountInput').value;
-    // turn into point number
-    var amountNumber = parseFloat(amountBefore);
+    // get string dollar amount & turn it into number
+    var amountBefore = parseFloat(document.getElementById('billAmountInput').value);
+    // turn into currency 
+    // THIS MIGHT NEED TO BE USED AFTER BECAUSE IT RETURNS A STRING
     // get tip amount
-    var tipAmount = document.querySelector('input[name="tips"]:checked').value
-    // turn into point number
-    var tipNumber = parseFloat(tipAmount);
-    // console log
-    console.log(tipNumber);
-    console.log(amountBefore);
-    // when calculate button is clicked, both variables will pass through an equation and create the variable for tip dollar
-    // getting tip amount based on total
-    var calculatedTip = amountNumber * tipNumber;
+    var tipAmount = parseFloat(document.querySelector('input[name="tips"]:checked').value)
+    console.log(tipAmount);
+    // // getting tip amount based on billed amount
+    var calculatedTip = amountBefore * tipAmount;
     console.log(calculatedTip);
     // tip amount will be added to dollar amount
-    var totalAmount = calculatedTip + amountNumber;
-    // getting total amount with tip
+    var totalAmount = calculatedTip + amountBefore;
+    // // getting total amount with tip
     console.log(totalAmount);
     // post tip into p tag
-    if (totalAmount > 0) {
-        var tipText = calculatedTip.toString();
-        document.getElementById("innerTipAmountId").innerHTML = `$${tipText}`
+    if (tipAmount > 0) {
+        var tipCurrency = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD' }).format(calculatedTip);
+        document.getElementById("innerTipAmountId").innerHTML = `${tipCurrency}`
     }
-    // post total into p tag
-    if (calculatedTip > 0) {
-        var totalText = totalAmount.toString();
-        document.getElementById("totalAmountId").innerHTML = `$${totalText}`
+    // // post total into p tag
+    if (totalAmount > 0) {
+        var amountCurrency = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD' }).format(totalAmount);
+        document.getElementById("totalAmountId").innerHTML = `${amountCurrency}`
     }
     // both amounts will be displayed on the screen
 
