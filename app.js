f75939ba131abc28605afa2d6a9bef15b79d599d
Hannah <hannahbrittany@hotmail.com>
2021-06-02 20:26:45 -0400

    innerhtml error fixed.

--- app.js
+++ app.js
@@ -10,18 +10,25 @@ function getTip() {
     // turn into point number
     var tipNumber = parseFloat(tipAmount);
     // console log
-    console.log(tipNumber)
+    console.log(tipNumber);
     console.log(amountNumber);
     // when calculate button is clicked, both variables will pass through an equation and create the variable for tip dollar
     // getting tip amount based on total
     var calculatedTip = amountNumber * tipNumber;
     console.log(calculatedTip);
     // tip amount will be added to dollar amount
-    // getting total amount with tip
     var totalAmount = calculatedTip + amountNumber;
+    // getting total amount with tip
     console.log(totalAmount);
+    // post tip into p tag
     if (totalAmount > 0) {
-        document.getElementById("innerTipAmount").innerHTML = `$${calculatedTip.toString}`
+        var tipText = calculatedTip.toString();
+        document.getElementById("innerTipAmountId").innerHTML = `$${tipText}`
+    }
+    // post total into p tag
+    if (calculatedTip > 0) {
+        var totalText = totalAmount.toString();
+        document.getElementById("totalAmountId").innerHTML = `$${totalText}`
     }
     // both amounts will be displayed on the screen
 
